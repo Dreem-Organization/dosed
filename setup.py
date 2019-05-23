d905c564af0f2105c5aff86beb73b6d44eebaea7
Valentino <v.thorey@gmail.com>
2019-05-23 10:38:58 +0200

    setup.py packages fixed

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Dreem-Organization/dosed/",
-    packages=["dosed"],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
