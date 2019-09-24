3162161d47bc74053760b90f9290454aebec6fe5
Theo Moutakanni <theo.moutakanni@free.fr>
2019-09-24 11:32:06 -0400

    Revert "[events] add the possibility to load events from json"
    This reverts commit 2ff757111f268fdd74bca9060ee6435591087eed.

--- dosed/utils/data_from_h5.py
+++ dosed/utils/data_from_h5.py
@@ -3,8 +3,6 @@
 import numpy as np
 
 import h5py
-import json
-import os
 
 from ..preprocessing import normalizers
 from scipy.interpolate import interp1d
@@ -29,34 +27,12 @@ def get_h5_data(filename, signals, fs):
 
 
 def get_h5_events(filename, event, fs):
-    if "json_path" in event:
-        directory, filename = os.path.split(filename)
-        filename = os.path.join(directory, event["apnea"], filename[:-3] + ".json")
-        with open(filename) as f:
-            f = json.load(f)
-            starts = []
-            durations = []
-            for event in f["labels"]:
-                starts.append(event["start"])
-                durations.append(event["end"] - event["start"])
+    with h5py.File(filename, "r") as h5:
+        starts = h5[event["h5_path"]]["start"][:]
+        durations = h5[event["h5_path"]]["duration"][:]
+        assert len(starts) == len(durations), "Inconsistents event durations and starts"
 
-            assert len(starts) == len(durations), "Inconsistents event durations and starts"
-
-            data = np.zeros((2, len(starts)))
-            data[0, :] = np.array(starts) * fs
-            data[1, :] = np.array(durations) * fs
-
-    elif "h5_path" in event:
-        with h5py.File(filename, "r") as h5:
-            starts = h5[event["h5_path"]]["start"][:]
-            durations = h5[event["h5_path"]]["duration"][:]
-            assert len(starts) == len(durations), "Inconsistents event durations and starts"
-
-            data = np.zeros((2, len(starts)))
-            data[0, :] = starts * fs
-            data[1, :] = durations * fs
-
-    else:
-        raise Exception("No events' path given !")
-
+        data = np.zeros((2, len(starts)))
+        data[0, :] = starts * fs
+        data[1, :] = durations * fs
     return data
