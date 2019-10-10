c47f7d8df7e91aaaf46199af7e23c5bbcc45819e
Theo Moutakanni <theo.moutakanni@free.fr>
2019-10-10 08:48:20 -0400

    [tests] Add seed for test_trainers.py

--- tests/test_trainers.py
+++ tests/test_trainers.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import random
 
 from dosed.datasets import BalancedEventDataset
 from dosed.models import DOSED4
@@ -10,6 +11,7 @@ def test_full_training():
     seed = 42
     torch.manual_seed(seed)
     np.random.seed(seed)
+    random.seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
